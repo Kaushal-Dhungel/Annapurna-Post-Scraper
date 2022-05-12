fedfc556d757660f1beb27435d4c4c1209b4a166
Kaushal-Dhungel <hazardfanboy91@gmail.com>
2022-05-12 10:56:16 +0545

    some improvements

--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
+import json
 import requests
+import os
 
 endpoint = "https://bg.annapurnapost.com/api/search?title="
 
@@ -7,9 +9,9 @@ def scraper(url:str):
     returns scrapped Annapurna Post's articles
     """
     response = requests.get(url)
-   
+    
     # if any error occurs
-    if response.status_code == 400:
+    if response.status_code == 404:
         return []
     
     # access the actual data we need
@@ -18,32 +20,53 @@ def scraper(url:str):
 
 
 def scrape(word:str):
-
     url = endpoint + word 
-    result = scraper(url= url)
-
-    # no items returned after searching
-    if len(result) == 0:
-        return result
+    filename = word + ".json"
+    pages_read = None
+   
+    # the script has been already run before
+    if os.path.exists(filename):
+        with open(filename, mode='r', encoding='utf-8') as file:
+            json_file = json.loads(file.read())
+            pages_read = json_file.get('pages_read')
 
     # for pagination
-    i = 2    
+    result = []
+    i = 1   
+   
+    # if we had previously run the script
+    if pages_read:
+        i = int(pages_read) + 1
+
     while True:
-        url = endpoint + word + "&page=" + str(i)
+        if i > 1:
+            url = endpoint + word + "&page=" + str(i)
+        
+        print("url is ---------------", url)
         items = scraper(url)
         
-        # if after pagination no more search result esixts OR it total items exceeds 30 then break the loop
+        # if no search result exists / if after pagination no more search result esixts OR it total no of items exceeds 30 then break the loop
         if len(items) == 0 or len(result) >= 30:
             break
         
         result += items
+        i += 1
 
-    return result
+    # json will be saved in this format
+    value = {
+        "pages_read" : i-1 if len(result)<30 else i,
+        "data" : result
+    }
+
+    # if we do not get any searchresult then no need to create a json file
+    if not len(result) == 0:
+        with open(filename, 'w') as file:
+            json.dump(value,file, indent=4)
+
+    return value
 
 
 result = scrape('काङ्ग्रेस')
 # result = scrape('lol')
-
-# print(len(result))
-
-print(result)
+print(len(result['data']))
+# print(result)
