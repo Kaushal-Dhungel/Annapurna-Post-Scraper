a61cb8effa4433d7a92ec6a0408a8b3cf325e9c1
Kaushal-Dhungel <hazardfanboy91@gmail.com>
2022-05-16 09:43:24 +0545

    updated

--- dict_validator.py
+++ dict_validator.py
@@ -1,58 +1,42 @@
 
+
 class ValiDict:
     
     def __init__(self,schema_dict):
         self.schema_dict = schema_dict
     
     def _evaluate(self, prop_dict, value):
+        data_type = prop_dict.get('type')
 
-            if prop_dict["type"] == str:
-                min_len = prop_dict.get('min_len')
-                max_len = prop_dict.get('max_len')
+        if data_type != type(value):  
+            return False
 
-                if not isinstance(value, str):
-                    return False
+        if data_type == str or data_type == int:
 
-                if min_len and len(value) < min_len:
-                    return False
+            min_val = prop_dict.get('min_len' if data_type == str else 'gt')
+            max_val = prop_dict.get('max_len' if data_type == str else 'lt')
 
-                if max_len and len(value) > max_len:
-                    return False
-            
-            if prop_dict["type"] == int:
-                lt = prop_dict.get('lt')
-                gt = prop_dict.get('gt')
+            if min_val and (len(value) if data_type == str else value) < min_val:
+                return False
 
-                if not isinstance(value, int):
-                    return False
+            if max_val and (len(value) if data_type == str else value) > max_val:
+                return False        
 
-                if lt and value > lt:
-                    return False
-
-                if gt and value < gt:
-                    return False
-
-            if prop_dict["type"] == bool:
-                t_f = prop_dict['t/f']
-
-                if not isinstance(value, bool):
-                    return False
-
-                if t_f and t_f != value:
-                    return False
+        if data_type == bool:
+            t_f = prop_dict['t/f']
+            if t_f and t_f != value:
+                return False
                 
-            return True
+        return True
 
     def validate(self, vali_dict:dict):
         
         for key, val in vali_dict.items():
             prop_dict = self.schema_dict.get(key)
             print(prop_dict)
-            if not prop_dict:
-                return False
-
-            if self._evaluate(prop_dict,val) == False:
-                return False
+            if prop_dict is not None:
+                if self._evaluate(prop_dict,val) == False:
+                    return False
 
         return True
 
@@ -61,7 +45,7 @@ schema_dict = {
     "name" : {
         "type":str,
         "min_len":2,
-        "max_len":5
+        "max_len":8
     },
 
     "age" : {
@@ -73,13 +57,14 @@ schema_dict = {
     "is_married":{
         "type":bool,
         "t/f":True
-    }
+    },
 }
 
 my_dict = {
-    "name" : "Kaus",
+    "name" : "Kau",
     "age" : 22,
-    "is_married":True
+    "is_married":True,
+    "my_list":[1,2,3]
 }
 
 v = ValiDict(schema_dict)
